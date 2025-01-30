3c4aa4208a84d09c6ac73b826cdbb8753785c7c5
Diogo Pina <diogojpina@gmail.com>
2025-01-30 11:07:53 -0300

    Add initial style

--- frontend/src/pages/home/home.page.tsx
+++ frontend/src/pages/home/home.page.tsx
@@ -3,6 +3,10 @@ import UrlUtil from "../../util/url.util";
 import { ShortenerService } from "../../services";
 import { ShortUrl } from "../../entities";
 
+import "./home.page.scss";
+import { TbLayersLinked } from "react-icons/tb";
+import { IoCopy } from "react-icons/io5";
+
 const HomePage = () => {
   const [url, setUrl] = useState("");
   const [shortUrl, setShortUrl] = useState<ShortUrl | null>(null);
@@ -26,29 +30,48 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <h1>Url Shortener</h1>
-      <h2>Enter the URL to shorten</h2>
-      <form onSubmit={shorten}>
-        <div>
-          <label>URL</label>
-          <input
-            type="text"
-            value={url}
-            onChange={(event) => setUrl(event.target.value)}
-          />
+    <div className="container-url-shortner">
+      <div className="box">
+        <div className="title">
+          <h1>URL Shortener</h1>
+          <TbLayersLinked />
         </div>
-        <button type="submit">Shorten</button>
-      </form>
+        <span className="subtitle">Enter the URL to shorten</span>
 
-      {shortUrl && (
-        <>
-          {shortUrl.short}
-          <button onClick={() => navigator.clipboard.writeText(shortUrl.short)}>
-            Copy
-          </button>
-        </>
-      )}
+        <form onSubmit={shorten}>
+          <div>
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              id="url"
+              value={url}
+              onChange={(event) => setUrl(event.target.value)}
+            />
+          </div>
+          <div className="button">
+            <button type="submit">Shorten</button>
+          </div>
+        </form>
+
+        {shortUrl && (
+          <div className="results">
+            <span className="success-message">
+              {" "}
+              Success! Here's your short URL:
+            </span>
+            <div className="shortened-url">
+              <a href={shortUrl.short} target="_blank">
+                {shortUrl.short}
+              </a>
+              <button
+                onClick={() => navigator.clipboard.writeText(shortUrl.short)}
+              >
+                <IoCopy /> Copy
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
